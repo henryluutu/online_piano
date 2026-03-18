38c9b75e6ca0c0de80d313cca3f1ea5e7895a27d
henryluutu <henryluutu@gmail.com>
2026-03-18 08:01:34 +0300

    Configure web, Android, and Windows build targets

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.henryluutu.onlinepiano",
+  appName: "Online Piano",
+  webDir: "out",
+  bundledWebRuntime: false,
+};
+
+export default config;
